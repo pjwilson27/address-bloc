f6ec351ac058a433714094d60cacedc8a1adc048
Phil Wilson <pjwilson27@gmail.com>
2017-02-09 02:06:56 +0000

    Completed Assignment 5

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -17,6 +17,24 @@
        expect(book.entries.size).to eq(0)
       end
     end
+    
+    describe "#remove_entry" do
+        it "removes an entry using the name, phone_number, and email address" do
+            book = AddressBook.new
+            book.add_entry('Bobby Boucher', '614.123.5555', 'boosh@waterboy.com')
+            
+            name = "Ada Lovelace"
+            phone_number = "010.012.1815"
+            email = "augusta.king@lovelace.com"
+            book.add_entry(name,phone_number,email)
+            
+            expect(book.entries.size).to eq(2)
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            expect(book.entries.size).to eq(1)
+            expect(book.entries.first.name).to eq("Bobby Boucher")
+        end
+        end
+
 
    describe "#add_entry" do
      it "adds only one entry to the address book" do
