3b2a338c430519a17337d619641b0e3ba0795ff2
Phil Wilson <pjwilson27@gmail.com>
2017-02-14 11:19:00 +0000

    Completed Assignment 8

--- models/address_book.rb
+++ models/address_book.rb
@@ -70,4 +70,14 @@
      #5
      return nil
    end
+   def iterative_search(name)
+    entries.each do |entry|
+     if entry.name == name
+      return entry
+     end
+    end
+    
+    return nil
+    
+   end
    end
